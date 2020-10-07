268b2625b5a0f47a8c9ac6c74892753204ae029c
dark-flames <dark_flames@outlook.com>
2020-10-08 02:21:53 +0900

    feat: use unsafe to implement skiplist

--- src/skip_list/mod.rs
+++ src/skip_list/mod.rs
@@ -1,13 +1,14 @@
+use crate::block::Block;
 use crate::interface::Key;
 use crate::skip_list::level_generator::LevelGenerator;
-use crate::skip_list::node::SkipListNode;
+use crate::skip_list::node::Node;
 
 mod level_generator;
 mod node;
 
 #[allow(dead_code)]
 pub struct SkipList<'pool, K: Key> {
-    entry: Box<SkipListNode<'pool, K>>,
+    entry: *mut Node<'pool, K>,
     size: usize,
     level_generator: &'static dyn LevelGenerator,
 }
@@ -15,10 +16,71 @@ pub struct SkipList<'pool, K: Key> {
 #[allow(dead_code)]
 impl<'pool, K: Key> SkipList<'pool, K> {
     pub fn new(level_generator: &'static dyn LevelGenerator) -> SkipList<'static, K> {
+        let head = Box::new(Node::head());
         SkipList {
-            entry: Box::new(SkipListNode::Head),
+            entry: Box::into_raw(head),
             size: 0,
             level_generator,
+        }
+    }
+
+    pub fn insert(&mut self, block: &'pool Block<K>) {
+        let node = Box::into_raw(Box::new(Node::new(block)));
+        let level = self.level_generator.generate_level();
+
+        let mut prev = self.find_position(node);
+
+        while prev.len() > level + 1 {
+            prev.pop();
+        }
+
+        while prev.len() < level + 1 {
+            prev.push(self.entry);
+        }
+
+        for (level, prev_node) in prev.into_iter().enumerate() {
+            unsafe {
+                let next = (*prev_node).next[level];
+                (*node).add_level(next);
+                (*prev_node).next[level] = node;
+            };
+        }
+    }
+
+    fn find_position(&mut self, node: *mut Node<'pool, K>) -> Vec<*mut Node<'pool, K>> {
+        let mut prev = vec![];
+        let mut current = self.entry;
+        let mut current_level = self.max_level();
+
+        loop {
+            let next = unsafe { (*current).next.get(current_level).copied() };
+
+            match next {
+                Some(next_node) if next_node < node => {
+                    current = next_node;
+                }
+                _ => {
+                    prev.push(current);
+
+                    if current_level == 0 {
+                        break;
+                    } else {
+                        current_level -= 1;
+                        current = unsafe { (*current).next[current_level] };
+                    }
+                }
+            }
+        }
+
+        prev.reverse();
+
+        prev
+    }
+
+    fn max_level(&self) -> usize {
+        match unsafe { (*self.entry).next.len() } {
+            0 => 0,
+            n => n - 1,
         }
     }
 }
--- src/skip_list/node.rs
+++ src/skip_list/node.rs
@@ -3,41 +3,53 @@ use crate::interface::Key;
 use std::cmp::Ordering;
 
 #[allow(dead_code)]
-pub enum SkipListNode<'pool, K: Key> {
-    Head,
-    ValueNode(Node<'pool, K>),
-}
-
-#[allow(dead_code)]
 pub struct Node<'pool, K: Key> {
-    pub block_ref: &'pool Block<'pool, K>,
-    pub key: &'pool K,
-    pub next: Vec<&'pool Node<'pool, K>>,
+    pub block: Option<&'pool Block<'pool, K>>,
+    pub next: Vec<*mut Node<'pool, K>>,
 }
 
 #[allow(dead_code)]
 impl<'pool, K: Key> Node<'pool, K> {
     pub fn new(block: &'pool Block<'pool, K>) -> Node<'pool, K> {
         Node {
-            block_ref: block,
-            key: <&K>::clone(&block.key),
+            block: Some(block),
             next: vec![],
         }
     }
 
-    pub fn add_level(&mut self, next: &'pool Node<'pool, K>) {
+    pub fn head() -> Node<'pool, K> {
+        Node {
+            block: None,
+            next: vec![],
+        }
+    }
+
+    pub fn is_head(&self) -> bool {
+        self.block.is_none()
+    }
+
+    pub fn add_level(&mut self, next: *mut Node<'pool, K>) {
         self.next.push(next)
     }
 }
 
 impl<'pool, K: Key> PartialEq for Node<'pool, K> {
     fn eq(&self, other: &Self) -> bool {
-        self.key == other.key
+        match (self.block, other.block) {
+            (Some(self_block), Some(other_block)) => self_block.key == other_block.key,
+            (None, None) => true,
+            _ => false,
+        }
     }
 }
 
 impl<'pool, K: Key> PartialOrd for Node<'pool, K> {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        self.key.partial_cmp(other.key)
+        match (self.block, other.block) {
+            (Some(self_block), Some(other_block)) => self_block.key.partial_cmp(other_block.key),
+            (None, Some(_)) => Some(Ordering::Less),
+            (Some(_), None) => Some(Ordering::Greater),
+            (None, None) => Some(Ordering::Equal),
+        }
     }
 }
