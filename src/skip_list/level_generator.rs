c772b542bcb7eacd49d8753b38be20b879f4cf53
dark-flames <dark_flames@outlook.com>
2020-10-08 11:20:45 +0900

    feat: random level generator

--- src/skip_list/level_generator.rs
+++ src/skip_list/level_generator.rs
@@ -1,5 +1,39 @@
+use rand::random;
+
 pub trait LevelGenerator {
     fn max_level(&self) -> usize;
 
     fn generate_level(&self) -> usize;
+}
+
+pub struct RandomLevelGenerator {
+    max_level: usize,
+    p: f32,
+}
+
+#[allow(dead_code)]
+impl RandomLevelGenerator {
+    pub fn new(max_level: usize, p: f32) -> RandomLevelGenerator {
+        RandomLevelGenerator { max_level, p }
+    }
+}
+
+impl LevelGenerator for RandomLevelGenerator {
+    fn max_level(&self) -> usize {
+        self.max_level
+    }
+
+    fn generate_level(&self) -> usize {
+        let mut level = 0;
+
+        while level < self.max_level() {
+            if random::<f32>() <= self.p {
+                level += 1;
+            } else {
+                break;
+            }
+        }
+
+        level
+    }
 }
